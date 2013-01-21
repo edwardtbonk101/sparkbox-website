c52891caeda904c3dca866002ade21aec58c1df3
Eddie Bonk <eddie@heysparkbox.com>
2013-01-21 11:52:04 -0500

    Created Eddie-Bonk.php and edited the team-nav.php page.

--- content-prototype/team-nav.php
+++ content-prototype/team-nav.php
@@ -4,7 +4,7 @@
     <li><a title="Link reveals person's information (hiding team overview)." href="team-member.php">Ben</a></li>
     <li><a title="Link reveals person's information (hiding team overview)." href="team-member.php">Dom</a></li>
     <li><a title="Link reveals person's information (hiding team overview)." href="team-member.php">Drew</a></li>
-    <li><a title="Link reveals person's information (hiding team overview)." href="team-member.php">Eddie</a></li>
+    <li><a title="Link reveals person's information (hiding team overview)." href="Eddie-Bonk.php">Eddie</a></li>
     <li><a title="Link reveals person's information (hiding team overview)." href="team-member.php">Ethan</a></li>
     <li><a title="Link reveals person's information (hiding team overview)." href="team-member.php">Jake</a></li>
     <li><a title="Link reveals person's information (hiding team overview)." href="team-member.php">J&auml;n</a></li>
